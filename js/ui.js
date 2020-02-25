18c55823ba070470b3bcee58d29f95c4a1842d6d
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-02-25 14:43:36 -0500

    Added manifest file and styles

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,6 @@
 // Initialize side-nav
 const sideNav = document.querySelector('.side-menu');
-M.Sidenav.init(sideNav, {});
+M.Sidenav.init(sideNav, { edge: 'right' });
+
+const sideFormAdd = document.querySelector('.side-form-add');
+M.Sidenav.init(sideFormAdd, {});
