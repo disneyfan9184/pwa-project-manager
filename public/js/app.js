af991f57464909cb80e7e120fd50c657dd4a9d49
Jerome Doran <disneyfan9184@gmail.com>
2020-03-22 17:28:22 -0400

    updated service worker

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // Check to see if the browser supports service workers
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('sw.js')
     .then(reg => console.log('Service worker registered', reg))
     .catch(error => console.log('Service worker not registered', error));
 }
